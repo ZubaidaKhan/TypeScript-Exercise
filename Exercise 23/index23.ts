5fc0cf14b7628f73a62201559f2a0bf6d49b4df4
ZubaidaKhan <141108671+ZubaidaKhan@users.noreply.github.com>
2023-08-15 22:27:31 -0700

    typescript tasks

---
+++ Exercise 23/index23.ts
@@ -0,0 +1,46 @@
+/*Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+*/
+
+let car:string='subaru';
+console.log("Is car=='subaru'? I predict true");
+console.log(car=='subaru');
+
+console.log("Is car=='Honda'? I predict false");
+console.log(car=='honda');
+
+console.log("Is car!=='toyota'? I predict true");
+console.log(car!=='toyota');
+
+
+console.log("Is car!=='Subaru'? I predict true");
+console.log(car!=='Subaru');
+
+
+console.log("Is car=='mazda'? I predict false");
+console.log(car=='mazda');
+
+let age:number=55;
+console.log('Is age>18 I predict true');
+console.log(age>18);
+
+console.log('Is age<25 I predict false');
+console.log(age<25);
+
+
+
+
+
+
+
+
+
